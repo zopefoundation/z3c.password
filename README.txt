2b58eb9702457297c31379065fe69bde7bb9e408
Roger Ineichen <roger@projekt01.ch>
2007-11-16 04:41:22 +0000

    Added setup files

---
+++ README.txt
@@ -0,0 +1,4 @@
+This package provides an API and implementation of a password generation and
+verification utility. A high-security implementation is provided that is
+suitable for banks and other high-security institutions. The package also
+offers a field and a property for those fields.
---
+++ setup.py
@@ -0,0 +1,69 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+
+$Id:$
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup (
+    name='z3c.password',
+    version='0.5.0dev',
+    author = "Stephan Richter, Roger Ineichen and the Zope Community",
+    author_email = "zope3-dev@zope.org",
+    description = "Password password generation and verification for Zope3",
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license = "ZPL 2.1",
+    keywords = "zope3 z3c password verification",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://cheeseshop.python.org/pypi/z3c.password',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c'],
+    extras_require = dict(
+        test = [
+            'z3c.coverage',
+            'zope.app.authentication',
+            'zope.app.testing',
+            'zope.testing',
+            ],
+        ),
+    install_requires = [
+        'setuptools',
+        'zope.component',
+        'zope.exceptions',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.schema',
+        ],
+    zip_safe = False,
+)
