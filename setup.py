dd6754731c1cda2c55bbeaeb8f937049bd4b1168
Michael Howitz <icemac@gmx.net>
2025-04-14 08:46:49 +0200

    Back to development: 3.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup(
     name='z3c.password',
-    version='3.0',
+    version='3.1.dev0',
     author="Stephan Richter, Roger Ineichen and the Zope Community",
     author_email="zope-dev@zope.dev",
     description="Password generation and verification utility for Zope3",
